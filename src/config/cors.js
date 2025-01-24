5d4a25ddde2cd5cf663d2595b97ed1112883896c
Hamza Zaheer <mhamzazaheer1@gmail.com>
2025-01-24 16:27:33 +0500

    allowd nextjs 3000 in cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -13,6 +13,7 @@ const corsConfig = () => {
 
       const additionalOrigins = [
         "http://localhost:5000",
+        "http://localhost:3000",
         "http://127.0.0.1:5000",
         BACKEND_URL,
       ];
